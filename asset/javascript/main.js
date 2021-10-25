0f35b644d7d6c666b8d72bf988ecf5bae95d1e98
DenuccioDiego <diegodenuccio95@gmail.com>
2021-10-25 15:45:53 +0200

    creazione array

---
+++ asset/javascript/main.js
@@ -0,0 +1,22 @@
+
+let posts = [
+
+     post1 = {
+          id : 1 ,
+          contenuto : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore voluptate deleniti ex?",
+          immagine : "https://unsplash.it/300/150?image=",
+          autore : "Philip Mangione" ,
+          numeroLikes : 80 ,
+          dataCreazione : "4 mesi fa",
+     },
+
+     post2 = {
+          id : 2 ,
+          contenuto : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore voluptate deleniti ex?",
+          immagine : "https://unsplash.it/300/150?image=",
+          autore : "Philip Mangione" ,
+          numeroLikes : 80 ,
+          dataCreazione : "4 mesi fa",
+     },
+
+];
