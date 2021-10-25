d3493554f4ef3483a9dee26f36df1ea426f29296
DenuccioDiego <diegodenuccio95@gmail.com>
2021-10-25 16:46:28 +0200

    struttura creata con js

--- asset/javascript/main.js
+++ asset/javascript/main.js
@@ -4,6 +4,7 @@ let posts = [
      post_1 = {
           id : 1 ,
           contenuto : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore voluptate deleniti ex?",
+          avatar : "https://www.placecage.com/50/50",
           immagine : "https://unsplash.it/300/150?image=",
           autore : "Philip Mangione" ,
           numeroLikes : 80 ,
@@ -12,11 +13,61 @@ let posts = [
 
      post_2 = {
           id : 2 ,
-          contenuto : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore voluptate deleniti ex?",
-          immagine : "https://unsplash.it/300/150?image=",
-          autore : "Philip Mangione" ,
-          numeroLikes : 80 ,
-          dataCreazione : "4 mesi fa",
+          contenuto : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat corrupti pariatur explicabo. Quis, aperiam molestiae! Laborum laboriosam neque dolorum molestias odit accusamus quidem quo!",
+          avatar : "https://placebeard.it/50x50",
+          immagine : "https://picsum.photos/300/150",
+          autore : "Sofia Perlari" ,
+          numeroLikes : 150 ,
+          dataCreazione : "2 mesi fa",
+     },
+
+     post_3 = {
+          id : 3 ,
+          contenuto : "Placeat corrupti pariatur explicabo. Laborum laboriosam neque dolorum molestias odit accusamus quidem quo!",
+          avatar : "https://www.fillmurray.com/50/50",
+          immagine : "https://placebear.com/300/150",
+          autore : "Diego Denuccio",
+          numeroLikes : 15 ,
+          dataCreazione : "15 giorni fa",
      },
 
 ];
+
+let = selezioneContainer = document.querySelector(".container")
+console.log(selezioneContainer)
+
+for (let i=0; i<posts.length; i++){
+     let singoloPost = posts[i]
+
+     selezioneContainer.insertAdjacentHTML('beforeend', `<div class="card w-50 mx-auto mb-5">
+     <div class="card-body">
+          <div class="d-flex">
+               <img src="${singoloPost.avatar}" class="h-25 rounded-circle me-4" alt="">
+               <div>
+                    <h5 class="card-title">${singoloPost.autore}</h5>
+                    <p class="card-text"><small class="text-muted">${singoloPost.dataCreazione}</small></p>
+               </div>
+          </div>
+          
+          <p class="card-text">${singoloPost.contenuto}</p>
+          
+     </div>
+     <img src="${singoloPost.immagine}" class="card-img-bottom h-50 p-3" alt="...">
+</div>`)
+     
+     
+     
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
